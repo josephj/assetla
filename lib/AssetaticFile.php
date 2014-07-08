d641837871dd30014dcf5a350cbee4f0a49500c5
josephj <josephj6802@gmail.com>
2014-07-09 00:22:12 +1000

    Apply TDD

---
+++ lib/AssetaticFile.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Normalize Coffee, JavaScript, SASS, SCSS, and CSS files
+ * to browser-readable JavaScript and CSS format according to their extension.
+ *
+ *     $file = new AssetaticFile('./assets/foo.coffee', false);
+ *     echo $file->output(); // Must be valid JavaScript format.
+ */
+class AssetaticFile
+{
+
+    public function __construct($path, $minify = false)
+    {
+    }
+
+    public function output()
+    {
+
+    }
+
+}
+
+?>
---
+++ test/AssetaticFileTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require './vendor/autoload.php';
+require './lib/AssetaticFile.php';
+
+class TestAsstaticFile extends PHPUnit_Framework_TestCase
+{
+    public $test_files = array('test/fixture/a.js', 'test/fixture/b.coffee');
+    public $result_files = array('test/fixture/a_result.js', 'test/fixture/b_result.js');
+
+    public function testOutput()
+    {
+        // JS
+        $value = new AssetaticFile($this->test_files[0]);
+        $value = $value->output();
+        $expected = file_get_contents($this->result_files[0]);
+        $this->assertEquals($value, $expected);
+
+        // Coffee
+        $value = new AssetaticFile($this->test_files[1]);
+        $value = $value->output();
+        $expected = file_get_contents($this->result_files[1]);
+        $this->assertEquals($value, $expected);
+    }
+}
+
