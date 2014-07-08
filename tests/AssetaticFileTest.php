bcd4b2eeb3bba72750c84aa9380aff5657a323c2
josephj <josephj6802@gmail.com>
2014-07-09 08:07:04 +1000

    Ignore .bundle folder

--- tests/AssetaticFileTest.php
+++ tests/AssetaticFileTest.php
@@ -5,16 +5,17 @@ require './src/AssetaticFile.php';
 
 class AsstaticFileTest extends PHPUnit_Framework_TestCase
 {
-    public $test_files = array('test/fixture/a.js', 'test/fixture/b.coffee');
-    public $result_files = array('test/fixture/a_result.js', 'test/fixture/b_result.js');
+    public $test_files   = array('tests/fixture/a.js', 'tests/fixture/b.coffee');
+    public $result_files = array('tests/fixture/a_result.js', 'tests/fixture/b_result.js');
 
     public function testOutput()
     {
         // JS
-        $value = new AssetaticFile($this->test_files[0]);
+        $file = $this->test_files[0];
+        $value = new AssetaticFile($file);
         $value = $value->output();
         $expected = file_get_contents($this->result_files[0]);
-        $this->assertEquals($value, $expected);
+        $this->assertEquals($expected, $value);
 
         // Coffee
         $value = new AssetaticFile($this->test_files[1]);
