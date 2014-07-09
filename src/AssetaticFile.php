0204cf646980e18335f638fc13f6833bdc1acb02
josephj <josephj6802@gmail.com>
2014-07-10 08:29:18 +1000

    Add Coffee compile and UglifyJS

--- src/AssetaticFile.php
+++ src/AssetaticFile.php
@@ -7,17 +7,96 @@
  *     $file = new AssetaticFile('./assets/foo.coffee', false);
  *     echo $file->output(); // Must be valid JavaScript format.
  */
+
+require 'vendor/autoload.php';
+
+use Assetic\Asset\AssetCollection;
+use Assetic\Asset\FileAsset;
+use Assetic\Filter\CoffeeScriptFilter;
+use Assetic\Filter\UglifyJsFilter;
+
 class AssetaticFile
 {
+    private $file;
+    private $type;
+    private $filters;
+    private $config;
 
-    public function __construct($path, $minify = false)
+    public function __construct($path, $config = array())
     {
+        $this->file = self::find($path);
+        $this->type = self::getType($this->file);
+        $this->filters = array();
+        $this->config = new StdClass();
+        $this->config->minify            = (isset($config['minify'])) ? $config['minify'] : false;
+        $this->config->coffeePath        = (isset($config['coffeePath'])) ? $config['coffeePath'] : '/usr/bin/coffee';
+        $this->config->sassPath          = (isset($config['sassPath'])) ? $config['sassPath'] : '/usr/bin/sass';
+        $this->config->uglifyJsPath      = (isset($config['uglifyJsPath'])) ? $config['uglifyJsPath'] : '/usr/bin/uglifyjs'; // Version 2
+        $this->config->uglifyCssPath     = (isset($config['uglifyCssPath'])) ? $config['uglifyCssPath'] : '/usr/bin/uglifycss';
+        $this->config->nodeCoffeePath    = (isset($config['nodeCoffeePath'])) ? $config['nodeCoffeePath'] : null;
+        $this->config->nodeSassPath      = (isset($config['nodeSassPath'])) ? $config['nodeSassPath'] : null;
+        $this->config->nodeUglifyJsPath  = (isset($config['nodeUglifyJsPath'])) ? $config['nodeUglifyJsPath'] : null;
+        $this->config->nodeUglifyCssPath = (isset($config['nodeUglifyCssPath'])) ? $config['nodeUglifyCssPath'] : null;
     }
 
-    public function output()
+    private static function find($path)
     {
-
+        $filePath = realpath($path);
+        if ($filePath && file_exists($filePath))
+        {
+            return $filePath;
+        }
+        throw new FileNotFoundException("File not found: " . $path);
     }
+
+    private static function getType($file)
+    {
+        $path_parts = pathinfo($file);
+        return $path_parts['extension'];
+    }
+
+    public function dump()
+    {
+        $file = array(new FileAsset($this->file));
+        $this->setTypeFilter($this->type);
+        if ($this->config->minify) {
+            $this->setMinifyFilter($this->type);
+        }
+        $result = new AssetCollection($file, $this->filters);
+        return $result->dump();
+    }
+
+    private function setTypeFilter($type)
+    {
+        $coffeePath = $this->config->coffeePath;
+        switch ($type) {
+            case 'scss':
+            case 'sass':
+                $this->filters[] = new SassFilter($this->config->sassPath, $this->config->nodeSassPath);
+            break;
+            case 'coffee':
+                $this->filters[] = new CoffeeScriptFilter($this->config->coffeePath, $this->config->nodeCoffeePath);
+            break;
+        }
+        return $this->filters;
+    }
+
+    private function setMinifyFilter($type)
+    {
+        switch ($type) {
+            case 'css':
+            case 'scss':
+            case 'sass':
+                $this->filters[] = new UglifyCssFilter($this->config->uglifyCssPath, $this->config->nodeUglifyCssPath);
+            break;
+            case 'js':
+            case 'coffee':
+                $this->filters[] = new UglifyJsFilter($this->config->uglifyJsPath, $this->config->nodeUglifyJsPath);
+            break;
+        }
+        return $this->filters;
+    }
+
 
 }
 
--- tests/AssetaticFileTest.php
+++ tests/AssetaticFileTest.php
@@ -5,23 +5,35 @@ require './src/AssetaticFile.php';
 
 class AsstaticFileTest extends PHPUnit_Framework_TestCase
 {
-    public $test_files   = array('tests/fixture/a.js', 'tests/fixture/b.coffee');
-    public $result_files = array('tests/fixture/a_result.js', 'tests/fixture/b_result.js');
+    private $test_files   = array('tests/fixture/a.js', 'tests/fixture/b.coffee');
+    private $result_files = array('tests/fixture/a_result.js', 'tests/fixture/b_result.js', 'tests/fixture/a.min.js');
 
-    public function testOutput()
+    public function testJsDump()
     {
-        // JS
         $file = $this->test_files[0];
         $value = new AssetaticFile($file);
-        $value = $value->output();
+        $value = $value->dump();
         $expected = file_get_contents($this->result_files[0]);
         $this->assertEquals($expected, $value);
+    }
 
-        // Coffee
-        $value = new AssetaticFile($this->test_files[1]);
-        $value = $value->output();
+    public function testMinJsDump()
+    {
+        $file = $this->test_files[0];
+        $value = new AssetaticFile($file, array('minify' => true, 'uglifyJsPath' => '/Users/josephj/.nvm/v0.10.10/bin/uglifyjs'));
+        $value = trim($value->dump());
+        $expected = trim(file_get_contents($this->result_files[2]));
+        $this->assertEquals($expected, $value);
+    }
+
+    public function testDumpCoffee()
+    {
+        $file = $this->test_files[1];
+        $value = new AssetaticFile($file, array('coffeePath' => '/Users/josephj/.nvm/v0.10.10/bin/coffee'));
+        $value = $value->dump();
         $expected = file_get_contents($this->result_files[1]);
-        $this->assertEquals($value, $expected);
+        $this->assertEquals($expected, $value);
     }
 }
+
 
