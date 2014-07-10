99ca589b413226e71add41cceee0fd2137436fff
josephj <josephj6802@gmail.com>
2014-07-10 19:40:14 +1000

    Add Coffee and minify test

--- tests/AssetaticFileTest.php
+++ tests/AssetaticFileTest.php
@@ -8,7 +8,7 @@ class AsstaticFileTest extends PHPUnit_Framework_TestCase
     private $test_files   = array('tests/fixture/a.js', 'tests/fixture/b.coffee');
     private $result_files = array('tests/fixture/a_result.js', 'tests/fixture/b_result.js', 'tests/fixture/a.min.js');
 
-    public function testJsDump()
+    public function testDumpJs()
     {
         $file = $this->test_files[0];
         $value = new AssetaticFile($file);
@@ -17,7 +17,7 @@ class AsstaticFileTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $value);
     }
 
-    public function testMinJsDump()
+    public function testDumpJsMin()
     {
         $file = $this->test_files[0];
         $value = new AssetaticFile($file, array('minify' => true, 'uglifyJsPath' => '/Users/josephj/.nvm/v0.10.10/bin/uglifyjs'));
@@ -34,6 +34,20 @@ class AsstaticFileTest extends PHPUnit_Framework_TestCase
         $expected = file_get_contents($this->result_files[1]);
         $this->assertEquals($expected, $value);
     }
+
+    public function testDumpCoffeeMin()
+    {
+        $file = $this->test_files[0];
+        $value = new AssetaticFile($file, array(
+            'minify' => false,
+            'uglifyJsPath' => '/Users/josephj/.nvm/v0.10.10/bin/uglifyjs',
+            'coffeePath' => '/Users/josephj/.nvm/v0.10.10/bin/coffee'
+        ));
+        $value = trim($value->dump());
+        $expected = trim(file_get_contents($this->result_files[2]));
+        $this->assertEquals($expected, $value);
+    }
+
 }
 
 
